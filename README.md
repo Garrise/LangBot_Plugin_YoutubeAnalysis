0f2b640676103a1c83511a2578548d9de1323fde
Garrise <dan19920316@gmail.com>
2024-12-02 08:26:01 +0800

    modified:   README.md 	new file:   config.py 	modified:   main.py

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# HelloPlugin
+# YoutubeAnalysis
 
 <!--
 ## 插件开发者详阅
 
 ### 开始
 
-此仓库是 LangBot 插件模板，您可以直接在 GitHub 仓库中点击右上角的 "Use this template" 以创建你的插件。  
+此仓库是 QChatGPT 插件模板，您可以直接在 GitHub 仓库中点击右上角的 "Use this template" 以创建你的插件。  
 接下来按照以下步骤修改模板代码：
 
 #### 修改模板代码
@@ -16,14 +16,14 @@
 - 修改`main.py`中的`@register`中的插件 名称、描述、版本、作者 等信息
 - 修改`main.py`中的`MyPlugin`类名为你的插件类名
 - 将插件所需依赖库写到`requirements.txt`中
-- 根据[插件开发教程](https://docs.langbot.app/plugin/dev/tutor.html)编写插件代码
+- 根据[插件开发教程](https://qchatgpt.rockchin.top/develop/plugin-dev.html)编写插件代码
 - 删除 README.md 中的注释内容
 
 
 #### 发布插件
 
 推荐将插件上传到 GitHub 代码仓库，以便用户通过下方方式安装。   
-欢迎[提issue](https://github.com/RockChinQ/LangBot/issues/new?assignees=&labels=%E7%8B%AC%E7%AB%8B%E6%8F%92%E4%BB%B6&projects=&template=submit-plugin.yml&title=%5BPlugin%5D%3A+%E8%AF%B7%E6%B1%82%E7%99%BB%E8%AE%B0%E6%96%B0%E6%8F%92%E4%BB%B6)，将您的插件提交到[插件列表](https://github.com/stars/RockChinQ/lists/qchatgpt-%E6%8F%92%E4%BB%B6)
+欢迎[提issue](https://github.com/RockChinQ/QChatGPT/issues/new?assignees=&labels=%E7%8B%AC%E7%AB%8B%E6%8F%92%E4%BB%B6&projects=&template=submit-plugin.yml&title=%5BPlugin%5D%3A+%E8%AF%B7%E6%B1%82%E7%99%BB%E8%AE%B0%E6%96%B0%E6%8F%92%E4%BB%B6)，将您的插件提交到[插件列表](https://github.com/stars/RockChinQ/lists/qchatgpt-%E6%8F%92%E4%BB%B6)
 
 下方是给用户看的内容，按需修改
 -->
@@ -33,10 +33,17 @@
 配置完成 [LangBot](https://github.com/RockChinQ/LangBot) 主程序后使用管理员账号向机器人发送命令即可安装：
 
 ```
-!plugin get <你的插件发布仓库地址>
+!plugin get https://github.com/Garrise/LangBot_Plugin_YoutubeAnalysis
 ```
-或查看详细的[插件安装说明](https://docs.langbot.app/plugin/plugin-intro.html#%E6%8F%92%E4%BB%B6%E7%94%A8%E6%B3%95)
+或查看详细的[插件安装说明](https://qchatgpt.rockchin.top/develop/plugin-intro.html#%E6%8F%92%E4%BB%B6%E7%94%A8%E6%B3%95)
 
 ## 使用
+* 参考Google开发者文档，创建一个app并且打开v3 api，记下你的apikey  
+  [YouTube Data API Overview  |  Google Developers](https://developers.google.com/youtube/v3/getting-started)
 
+> 
+* 将apikey填入config.py中
+* 插件将会自动识别群聊里的YouTube视频链接并返回视频预览内容等等，包含以下两种：
+  * https://youtu.be/{id}
+  * https://www.youtube.com/watch?v={id}
 <!-- 插件开发者自行填写插件使用说明 -->
--- main.py
+++ main.py
@@ -1,49 +1,60 @@
 from pkg.plugin.context import register, handler, llm_func, BasePlugin, APIHost, EventContext
 from pkg.plugin.events import *  # 导入事件类
+import re
+import requests
+from pkg.platform.types import *
+from plugins.LangBot_Plugin_YoutubeAnalysis.config import Config
 
+'''
+当收到B站视频链接时，对B站链接进行分析并发送封面，标题，作者，等内容
+'''
 
-# 注册插件
-@register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
-class MyPlugin(BasePlugin):
-
-    # 插件加载时触发
+#注册插件
+@register(name='YoutubeAnalysis', description='当收到油管视频链接时，对该链接进行分析并发送封面，标题，作者，等内容', version='0.1', author="Garrise")
+class BiliAnalysisPlugin(BasePlugin):
+    #插件加载时触发
     def __init__(self, host: APIHost):
         pass
-
-    # 异步初始化
-    async def initialize(self):
-        pass
-
-    # 当收到个人消息时触发
-    @handler(PersonNormalMessageReceived)
-    async def person_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 PersonNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
-
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
-
-            # 回复消息 "hello, <发送者id>!"
-            ctx.add_return("reply", ["hello, {}!".format(ctx.event.sender_id)])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            ctx.prevent_default()
-
-    # 当收到群消息时触发
-    @handler(GroupNormalMessageReceived)
+    @handler(PersonMessageReceived)
+    @handler(GroupMessageReceived)
     async def group_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 GroupNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
-
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
-
-            # 回复消息 "hello, everyone!"
-            ctx.add_return("reply", ["hello, everyone!"])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            ctx.prevent_default()
-
-    # 插件卸载时触发
-    def __del__(self):
-        pass
+        msg = str(ctx.event.message_chain).strip()
+        # 如果msg含有youtube.com或https://youtu.be则截取视频id
+        match = re.search(r'www.youtube.com/watch\?v=([\w-]{11})', msg) or re.search(r'youtu.be/([\w-]{11})', msg)
+        if match:
+            yt_id = match.group(1)
+            print(f"Analyzing Youtube id: {yt_id}")
+            # 发送封面，标题，作者等信息
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'
+            }
+            response = requests.get(f"https://www.googleapis.com/youtube/v3/videos?id={yt_id}&key={Config.key}&part=snippet", headers=headers)
+            data = response.json()
+            if data['pageInfo']['totalResults'] != 0:
+                snippet = data['items'][0]['snippet']
+                title = snippet['title']
+                description = snippet['description']
+                channelTitle = snippet['channelTitle']
+                thumbnails = snippet['thumbnails']
+                publishedAt = snippet['publishedAt']
+                tagString = ""
+                tags = snippet['tags']
+                if tags:
+                    tagString = ", ".join(tags)
+                else:
+                    tagString = "无"
+                thumbnailUrl = thumbnails['maxres']['url'] if thumbnails['maxres'] else thumbnails['high']['url'] 
+                await ctx.send_message(ctx.event.launcher_type, str(ctx.event.launcher_id),MessageChain([
+                                                                                                        Image(url=thumbnailUrl),
+                                                                                                        f"频道：{channelTitle}\n",
+                                                                                                        f"标题：{title}\n",
+                                                                                                        f"时间：{publishedAt}\n",
+                                                                                                        f"链接：http://youtu.be/{yt_id}\n\n",
+                                                                                                        f"标签：{tagString}"
+                                                                                                        ]))
+                ctx.prevent_default()
+                ctx.prevent_postorder()
+            else:
+                await ctx.send_message(ctx.event.launcher_type, str(ctx.event.launcher_id),["视频解析失败"])
+                ctx.prevent_default()
+                ctx.prevent_postorder()
